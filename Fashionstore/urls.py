140509ee01b1983f3cf1e0cf36ef85204ac3907b
MahmudaAfifa <114296147+MahmudaAfifa@users.noreply.github.com>
2022-10-02 08:31:49 +0600

    Initial commit

---
+++ Fashionstore/urls.py
@@ -0,0 +1,32 @@
+"""Fashionstore URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import thinclude() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from django.contrib import admin
+from django.urls import path
+import home.views as home_views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('home/',home_views.home_view),
+    path('about/',home_views.about_view),
+    path('products/',home_views.product_view),
+    path('cart/',home_views.cart_view),
+    path('reviews/',home_views.reviews_view),
+    path('drop_ideas/',home_views.drop_idea_view),
+
+
+]
